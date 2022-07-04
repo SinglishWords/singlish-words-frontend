b704e168aeff8d87d51dfbc2747e77de242c1a41
swosw <89635081+swosw@users.noreply.github.com>
2022-07-04 16:01:07 +0800

    remove redirect
    restored form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import "./App.css";
 export class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <Form />
-      // </div>
-      <div>
-        This page has moved. Click <a href="https://nus.syd1.qualtrics.com/jfe/form/SV_8H3T02Xgkp8vY7s">here</a> to go to the new page.
-      </div>
+       <div className="App">
+         <Form />
+       </div>
     );
   }
 }
