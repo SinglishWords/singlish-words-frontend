fd5e871387de9154d4df336adaee3c9f8404e68e
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2022-07-19 18:00:32 +0800

    Add explore page buttons

--- src/components/Pages/Explore/Explore.js
+++ src/components/Pages/Explore/Explore.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
-import { Grid } from "@mui/material";
+import { Card, CardContent, Grid, TextField, IconButton } from "@mui/material";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import DownloadIcon from "@mui/icons-material/Download";
 import NavBar from "../../Helpers/NavBar/NavBar";
-// import { TextField } from "@mui/material";
 import "./Explore.css";
 
 export class Explore extends Component {
   render() {
     return (
-      <Grid container>
-        <NavBar />
-        <Grid container>
-          <Grid item>
-            <h1>Explore</h1>
-          </Grid>
+      <Grid container className="explore_page_container">
+        <Grid item xs={12}>
+          <NavBar />
+        </Grid>
+        <Grid item xs={12} className="card_container">
+          <Card elevation={3} className="card">
+            <CardContent>
+              <Grid item xs={12} className="inputs_container">
+                <Grid item xs={6}>
+                  <TextField label="Search" variant="outlined" />
+                </Grid>
+                <Grid item xs={6} className="btn_container">
+                  <IconButton className="download_btn">
+                    <DownloadIcon />
+                  </IconButton>
+                  <IconButton className="shuffle_btn">
+                    <ShuffleIcon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     );
